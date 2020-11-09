fc36a1b12b1406ecc9c6e7b6687b05057345dc3d
MaxBUhe <mb@shopmodule.com>
2020-11-09 18:38:29 +0100

    adopt module

---
+++ src/metadata.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+/**
+ * Module information
+ */
+$aModule = array(
+  'id'              => 'mb/basket',
+  'title'           => 'Basket features',
+  'description'     => '',
+  'thumbnail'       => 'picture.png',
+  'version'         => '1.0.0',
+  'author'          => 'Max Buhe',
+  'controllers'   => [],
+  'templates' => ['' => '',]
+);
